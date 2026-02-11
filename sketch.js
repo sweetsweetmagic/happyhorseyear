dc2d515372e659018033fbc7da04bc61b5cbb5e4
sweetsweetmagic <102903189+sweetsweetmagic@users.noreply.github.com>
2026-02-12 00:04:18 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+  textSize(20);
+  textAlign(CENTER);
+  text("happy horse year !",width/2,height/2);
+}
